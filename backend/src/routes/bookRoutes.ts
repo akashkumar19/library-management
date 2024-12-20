14ca56c5e3f916c22aeb2023e04af50be029b081
akash <akashkumar019@yahoo.com>
2024-12-20 16:35:17 +0000

    feat: create book routes

---
+++ backend/src/routes/bookRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { BookController } from '../controllers/bookController';
+import { paginationMiddleware } from '../middleware/paginationMiddleware';
+
+export const createBookRouter = (controller: BookController) => {
+    const router = Router();
+  
+    router.get('/', paginationMiddleware, controller.getBooks);
+    router.post('/', controller.addBook);
+    router.put("/:id", controller.updateBook);
+    router.delete("/:id", controller.deleteBook);
+    router.patch("/:id/borrow", controller.borrowBook);
+  
+    return router;
+  };
