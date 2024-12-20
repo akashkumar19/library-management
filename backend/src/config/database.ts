d883cf4034ff52de3f15f13d2bd0d2f93b6ff01a
akash <akashkumar019@yahoo.com>
2024-12-20 16:34:32 +0000

    feat: db setup

---
+++ backend/src/config/database.ts
@@ -0,0 +1,35 @@
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+const dbPath = process.env.DB_PATH || "./lms.db";
+export const initializeDatabase = async () => {
+  const db = await open({
+    filename: dbPath,
+    driver: sqlite3.Database,
+  });
+
+  await db.exec(`
+        CREATE TABLE IF NOT EXISTS borrowers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        phone TEXT NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        )`);
+
+  await db.exec(`
+            CREATE TABLE IF NOT EXISTS books (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              title TEXT NOT NULL,
+              author TEXT NOT NULL,
+              genre TEXT NOT NULL,
+              isbn TEXT UNIQUE NOT NULL,
+              available BOOLEAN DEFAULT 1,
+              borrower_id INTEGER,
+              borrow_date TEXT,
+              FOREIGN KEY (borrower_id) REFERENCES borrowers(id)
+            )
+          `);
+
+  return db;
+};
