742bc30b981b254aa7bd0db9280ac859018e31be
akash <akashkumar019@yahoo.com>
2024-12-31 11:11:02 +0000

    chore: minor refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter } from "react-router-dom";
-import "./App.scss";
 import { AppRoutes } from "./AppRoutes";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
